bdaf9e34d4449771d7715f548b27375187366386
Gabriel <al2012.163.torres@gmail.com>
2021-07-28 02:37:04 -0300

    Deck shuffle and distribution

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 function flipCard(selectedCard){
     selectedCard.classList.toggle("selected");
 }
+function shuffle() { 
+	return Math.random() - 0.5; 
+}
 function selectNumberOfCards(){
     let numberOfCards = 0;
     while (numberOfCards < 4 || numberOfCards > 14 || numberOfCards % 2 !== 0){
@@ -8,4 +11,32 @@ function selectNumberOfCards(){
     }
     return numberOfCards;
 }
-selectNumberOfCards();
+function prepareDeck(numberOfCards){
+    for(index = 0; index < numberOfCards; index++){
+        deck.push(`
+        <div class="card" onclick="flipCard(this)">
+            <div class="cover face">
+                <img src="/media/card-cover.png" alt="Parrot Card">
+            </div>
+            <div class="front face">
+                <img src="/media/gifs/card${Math.floor((index+2)/2)}.gif" alt="Parrot ${Math.floor((index+2)/2)}">
+            </div>
+        </div>
+        `);
+    }
+}
+function cardsDistribution(){
+    let cards = "";
+    for (index = 0; index < deck.length; index++){
+        cards += deck[index];
+    }
+    document.querySelector(".board").innerHTML = cards;
+}
+function prepareTheGame(){
+    const numberOfCards = selectNumberOfCards();
+    prepareDeck(numberOfCards);
+    deck.sort(shuffle);
+    cardsDistribution();
+}
+let deck = [];
+prepareTheGame();
