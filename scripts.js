f2f577c61235f0de1e3861877e06bcad523a5ead
Gabriel <al2012.163.torres@gmail.com>
2021-07-28 22:47:35 -0300

    Game machanics

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function flipCard(selectedCard){
     selectedCard.classList.toggle("selected");
 }
-function shuffle() { 
+function shuffle(){ 
 	return Math.random() - 0.5; 
 }
 function selectNumberOfCards(){
@@ -14,7 +14,7 @@ function selectNumberOfCards(){
 function prepareDeck(numberOfCards){
     for(index = 0; index < numberOfCards; index++){
         deck.push(`
-        <div class="card" onclick="flipCard(this)">
+        <div class="card" onclick="selectCard(this)">
             <div class="cover face">
                 <img src="/media/card-cover.png" alt="Parrot Card">
             </div>
@@ -37,6 +37,49 @@ function prepareTheGame(){
     prepareDeck(numberOfCards);
     deck.sort(shuffle);
     cardsDistribution();
+    cardsFlipped = 0;
 }
+function compareCards(){
+    if(firstCard.innerHTML === secondCard.innerHTML){
+        return true;
+    }
+    return false;
+}
+function resetCards(){
+    firstCard = "";
+    secondCard = "";
+}
+function checkIfGameOver(){
+    const allCards = document.querySelectorAll(".card");
+    const flippedCards = document.querySelectorAll(".selected");
+    if(allCards.length === flippedCards.length){
+        alert(`Você ganhou em ${cardsFlipped} jogadas!`);
+    }
+}
+function checkTurn(){
+    const isThePlayerRight = compareCards();
+    if(isThePlayerRight === false){
+        setTimeout(flipCard, 1000, firstCard);
+        setTimeout(flipCard, 1000, secondCard);
+    }
+    resetCards();
+    setTimeout(checkIfGameOver, 1000);
+}
+function selectCard(selectedCard){
+    if(selectedCard.classList.contains("selected") === false){
+        flipCard(selectedCard);
+        if(firstCard === ""){
+            firstCard = selectedCard;
+        }
+        else{
+            secondCard  = selectedCard;
+            checkTurn();
+        }
+        cardsFlipped++;
+    }
+}
+let firstCard = "";
+let secondCard = "";
 let deck = [];
+let cardsFlipped;
 prepareTheGame();
