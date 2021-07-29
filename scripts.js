3dfd3fddbcd0395663c8f310cdc5d1f2f26246fe
Gabriel <al2012.163.torres@gmail.com>
2021-07-29 01:15:03 -0300

    Refactoring (JS)

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-function shuffle(){ 
-	return Math.random() - 0.5; 
-}
 function selectNumberOfCards(){
     let numberOfCards = 0;
     while (numberOfCards < 4 || numberOfCards > 14 || numberOfCards % 2 !== 0){
@@ -10,23 +7,26 @@ function selectNumberOfCards(){
 }
 function prepareDeck(numberOfCards){
     deck = [];
-    for(index = 0; index < numberOfCards; index++){
+    for(i = 0; i < numberOfCards; i++){
         deck.push(`
-        <div class="card" onclick="selectCard(this)">
+        <div class="card ${i}" onclick="selectCard(this)">
             <div class="cover face">
                 <img src="/media/card-cover.png" alt="Parrot Card">
             </div>
             <div class="front face">
-                <img src="/media/gifs/card${Math.floor((index+2)/2)}.gif" alt="Parrot ${Math.floor((index+2)/2)}">
+                <img src="/media/gifs/card${Math.floor((i+2)/2)}.gif" alt="Parrot ${Math.floor((i+2)/2)}">
             </div>
         </div>
         `);
     }
 }
+function shuffle(){ 
+	return Math.random() - 0.5; 
+}
 function cardsDistribution(){
     let cards = "";
-    for (index = 0; index < deck.length; index++){
-        cards += deck[index];
+    for (i = 0; i < deck.length; i++){
+        cards += deck[i];
     }
     document.querySelector(".board").innerHTML = cards;
 }
@@ -42,9 +42,6 @@ function prepareTheGame(){
     cardsFlipped = 0;
     time = 0;
     timeout = setInterval(clock, 1000);
-}
-function flipCard(selectedCard){
-    selectedCard.classList.toggle("selected");
 }
 function compareCards(){
     if(firstCard.innerHTML === secondCard.innerHTML){
@@ -86,9 +83,13 @@ function checkTurn(){
     resetCards();
     checkIfGameOver();
 }
+function flipCard(selectedCard){
+    selectedCard.classList.toggle("selected");
+}
 function selectCard(selectedCard){
     if(selectedCard.classList.contains("selected") === false){
         flipCard(selectedCard);
+        cardsFlipped++;
         if(firstCard === ""){
             firstCard = selectedCard;
         }
@@ -96,7 +97,6 @@ function selectCard(selectedCard){
             secondCard  = selectedCard;
             checkTurn();
         }
-        cardsFlipped++;
     }
 }
 let firstCard = "";
