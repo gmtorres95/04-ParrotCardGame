c21513701972b20e64ddc3a0548dd1c5da5ae5ee
Gabriel <al2012.163.torres@gmail.com>
2021-08-02 01:38:13 -0300

    Refactoring 3 (JS)

--- scripts.js
+++ scripts.js
@@ -1,17 +1,30 @@
+//These functions will prepare the game for the player, starting at "prepareTheGame()"
+function resetGame(){
+    playerName = "";
+    numberOfCards = 0;
+    deck = [];
+    correctGuesses = 0;
+    cardsFlipped = 0;
+    time = 0;
+    firstCard = "";
+    secondCard = "";
+}
+function askForName(){
+    while(!playerName || playerName === ""){
+        playerName = prompt("Qual é o seu nome? (Máximo de 7 caracteres)");
+    }
+}
 function selectNumberOfCards(){
-    let numberOfCards = 0;
     while (numberOfCards < 4 || numberOfCards > 14 || numberOfCards % 2 !== 0){
         numberOfCards = Number(prompt("Selecione um nº par de cartas (4 - 14)"));
     }
-    return numberOfCards;
 }
 function prepareDeck(){
-    deck = [];
     for(i = 0; i < numberOfCards; i++){
         deck.push(`
-        <div class="card ${i}" onclick="selectCard(this)">
+        <div class="card" onclick="selectCard(this)">
             <div class="cover face">
-                <img src="/media/card-cover.png" alt="Parrot Card">
+                <img src="/media/card-cover.png" alt="Card Cover">
             </div>
             <div class="front face">
                 <img src="/media/gifs/card${Math.floor((i+2)/2)}.gif" alt="Parrot ${Math.floor((i+2)/2)}">
@@ -35,16 +48,16 @@ function clock(){
     document.querySelector(".time").innerHTML = "TEMPO: " + time;
 }
 function prepareTheGame(){
-    playerName = prompt("Qual é o seu nome? (Máximo de 7 caracteres)");
-    numberOfCards = selectNumberOfCards();
+    resetGame();
+    askForName();
+    selectNumberOfCards();
     prepareDeck();
     deck.sort(shuffle);
     cardsDistribution();
-    correctGuesses = 0;
-    cardsFlipped = 0;
-    time = 0;
     timeoutID = setInterval(clock, 1000);
 }
+
+//These functions are the game mechanics, starting at "selectCard()"
 function compareCards(){
     if(firstCard.innerHTML === secondCard.innerHTML){
         return true;
@@ -55,65 +68,17 @@ function resetCards(){
     firstCard = "";
     secondCard = "";
 }
-function checkIfPlayAgain(){
-    document.querySelector(".game-over").classList.add("hidden");
-    let wantToPlayAgain;
-    while (wantToPlayAgain !== "sim" && wantToPlayAgain !== "não"){
-        wantToPlayAgain = prompt("Deseja jogar novamente (sim/não)?");
-    }
-    if (wantToPlayAgain === "sim"){
-        prepareTheGame();
-    }
-}
-function calculateScore(){
-    return (5 * numberOfCards - time);
-}
-function sortHighScore(a, b) {
-    if (Number(a[0]) < Number(b[0])) {
-      return 1;
-    }
-    if (Number(a[0]) > Number(b[0])) {
-      return -1;
-    }
-    return 0;
-}
-function updateHighScore(){
-    let text = `<tr>
-                    <th>Pts</th>
-                    <th>Nome</th>
-                    <th>Cartas</th>
-                    <th>Tempo</th>
-                </tr>`;
-    for(i = 0; i < highScore.length && i < 10; i++){
-        text += `<tr>
-                    <td>${highScore[i][0]}</td>
-                    <td>${highScore[i][1]}</td>
-                    <td>${highScore[i][2]}</td>
-                    <td>${highScore[i][3]}</td>
-                </tr>`;
-    }
-    document.querySelector("table").innerHTML = text;
-}
-function saveHighScore(){
-    highScore.push([calculateScore(), playerName.substring(0,7), numberOfCards, time]);
-    highScore.sort(sortHighScore);
-    updateHighScore();
-}
-function gameOver(){
-    alert(`Você ganhou em ${cardsFlipped} jogadas (${time} segundos)!`);
-    saveHighScore();
-    document.querySelector(".game-over").classList.remove("hidden");
-}
 function checkIfGameOver(){
-    const allCards = document.querySelectorAll(".card");
-    if((allCards.length / 2 ) === correctGuesses){
+    if((numberOfCards / 2 ) === correctGuesses){
         clearInterval(timeoutID);
         setTimeout(gameOver, 1000);
     }
 }
+function flipCard(selectedCard){
+    selectedCard.classList.toggle("selected");
+}
 function checkTurn(){
-    const isThePlayerRight = compareCards();
-    if(isThePlayerRight === false){
+    if(!compareCards()){
         setTimeout(flipCard, 1000, firstCard);
         setTimeout(flipCard, 1000, secondCard);
     }
@@ -123,11 +88,8 @@ function checkTurn(){
     resetCards();
     checkIfGameOver();
 }
-function flipCard(selectedCard){
-    selectedCard.classList.toggle("selected");
-}
 function selectCard(selectedCard){
-    if(selectedCard.classList.contains("selected") === false){
+    if(!selectedCard.classList.contains("selected")){
         flipCard(selectedCard);
         cardsFlipped++;
         if(firstCard === ""){
@@ -139,14 +101,75 @@ function selectCard(selectedCard){
         }
     }
 }
-let firstCard = "";
-let secondCard = "";
-let deck;
-let numberOfCards;
+
+//These functions will end the game, starting at "gameOver()", and will update de HighScore board
+function calculateScore(){
+    return (5 * numberOfCards - time);
+}
+function logScore(){
+    return {
+        playerName: playerName.substring(0,7),
+        playerGameMode: numberOfCards,
+        playerGameTime: time,
+        playerScore: calculateScore(),
+    };
+}
+function sortScores(a, b) {
+    if (Number(a.playerScore) < Number(b.playerScore)) {
+      return 1;
+    }
+    if (Number(a.playerScore) > Number(b.playerScore)) {
+      return -1;
+    }
+    return 0;
+}
+function updateHighScore(){
+    let text = `<tr><th>Pts</th>
+                    <th>Nome</th>
+                    <th>Cartas</th>
+                    <th>Tempo</th>
+                </tr>`;
+    for(i = 0; i < highScore.length && i < 10; i++){
+        text += `<tr>
+                    <td>${highScore[i].playerScore}</td>
+                    <td>${highScore[i].playerName}</td>
+                    <td>${highScore[i].playerGameMode}</td>
+                    <td>${highScore[i].playerGameTime}</td>
+                </tr>`;
+    }
+    document.querySelector("table").innerHTML = text;
+}
+function saveScore(){
+    highScore.push(logScore());
+    highScore.sort(sortScores);
+    updateHighScore();
+}
+function gameOver(){
+    alert(`Você ganhou em ${cardsFlipped} jogadas (${time} segundos)!`);
+    saveScore();
+    document.querySelector(".game-over").classList.remove("hidden");
+}
+
+//This function will check if the player wants to play again and restarts the game
+function checkIfPlayAgain(){
+    document.querySelector(".game-over").classList.add("hidden");
+    let playerAnswer;
+    while (playerAnswer !== "sim" && playerAnswer !== "não"){
+        playerAnswer = prompt("Deseja jogar novamente (sim/não)?");
+    }
+    if (playerAnswer === "sim"){
+        prepareTheGame();
+    }
+}
+
 let playerName;
-let cardsFlipped;
-let correctGuesses;
+let numberOfCards;
+let deck;
 let time;
 let timeoutID;
+let firstCard;
+let secondCard;
+let cardsFlipped;
+let correctGuesses;
 let highScore = [];
 prepareTheGame();
